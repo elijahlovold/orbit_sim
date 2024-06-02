2896594d7e4dd683568fbbd9ced02ee961fd7fae
elijah lovold <elovold@cadenceneuro.com>
2024-06-01 17:16:14 -0700

    add planets

--- celestial_bodies.hpp
+++ celestial_bodies.hpp
@@ -41,61 +41,87 @@ enum cel_type {
 
 class celestial {
     public: 
-        static constexpr int rec_decmi = 10;    // how many samples to decimate before recording
-        static constexpr double ep = 1000;
-
         cel_type type;
         std::string name;
         std::string color;
 
         double m; 
-        
+        double drag;
+
         double x;
         double y;
         double z;
 
-        double dx_dt;
-        double dy_dt;
-        double dz_dt;
-
-        double dx2_2dt;
-        double dy2_2dt;
-        double dz2_2dt;
-
-
+        bool record;
         int inc;
         std::vector<double> history_x_coords;
         std::vector<double> history_y_coords;
         std::vector<double> history_z_coords;
 
         celestial(std::string ic_name, double ic_m, double ic_x, double ic_y, double ic_z, double ic_dx_dt, double ic_dy_dt, double ic_dz_dt);
+        celestial(std::string ic_name, double ic_M, double ic_m, double ic_r);
 
         void accum_accel(celestial *cj); 
         void step_position(std::vector<celestial *> c_);
+        void step_position(std::vector<celestial *> c_, double *origin_x, double *origin_y, double *origin_z); 
+        void set_xyz(double set_x, double set_y, double set_z); 
+        void set_dxyzdt(double dx_dt, double dy_dt, double dz_dt);
+
+        private: 
+            static constexpr int rec_decmi = 100;    // how many samples to decimate before recording
+            static constexpr double ep = 1000;
+            // static constexpr double ep = 100;
+
+            double dx_dt;
+            double dy_dt;
+            double dz_dt;
+
+            double dx2_2dt;
+            double dy2_2dt;
+            double dz2_2dt;
 
 };
 
+celestial::celestial(std::string ic_name, double ic_M, double ic_m, double ic_r) {
+    this->name = ic_name;
+    this->color = "def";
 
-// double celestial::compute_grav_attract_pp(double r, double m0, double m1) {
-//     return G*m0*m1/(r*r);
-// }
+    this->m = ic_m;
+    this->drag = 0;
+
+    this->set_xyz(ic_r, 0, 0);
+    this->set_dxyzdt(0, sqrt(G*ic_M/ic_r), 0);
+
+    this->inc = 0;
+    this->record = true;
+}
 
 celestial::celestial(std::string ic_name, double ic_m, double ic_x, double ic_y, double ic_z, double ic_dx_dt, double ic_dy_dt, double ic_dz_dt) {
     this->name = ic_name;
     this->color = "def";
 
     this->m = ic_m;
+    this->drag = 0;
 
-    this->x = ic_x;
-    this->y = ic_y;
-    this->z = ic_z;
-
-    this->dx_dt = ic_dx_dt;
-    this->dy_dt = ic_dy_dt;
-    this->dz_dt = ic_dz_dt;
+    this->set_xyz(ic_x, ic_y, ic_z);
+    this->set_dxyzdt(ic_dx_dt, ic_dy_dt, ic_dz_dt);
 
     this->inc = 0;
+    this->record = true;
 }
+
+void celestial::set_xyz(double set_x, double set_y, double set_z) {
+    this->x = set_x;
+    this->y = set_y;
+    this->z = set_z;
+}
+
+void celestial::set_dxyzdt(double dx_dt, double dy_dt, double dz_dt) {
+    this->dx_dt = dx_dt;
+    this->dy_dt = dy_dt;
+    this->dz_dt = dz_dt;
+}
+
 
 void celestial::accum_accel(celestial *cj) {
     double x_diff = this->x - cj->x;
@@ -113,6 +139,34 @@ void celestial::accum_accel(celestial *cj) {
     this->dx2_2dt -= G*cj->m*x_diff/r_pow_3; 
     this->dy2_2dt -= G*cj->m*y_diff/r_pow_3; 
     this->dz2_2dt -= G*cj->m*z_diff/r_pow_3; 
+}
+
+void celestial::step_position(std::vector<celestial *> c_, double *origin_x, double *origin_y, double *origin_z) {
+    // reset accelerations
+    this->dx2_2dt = 0; 
+    this->dy2_2dt = 0; 
+    this->dz2_2dt = 0; 
+
+    // sum accelerations
+    for (celestial *ci : c_) {
+        this->accum_accel(ci);
+    }
+
+    // step velocities
+    this->dx_dt += this->dx2_2dt*ep;
+    this->dy_dt += this->dy2_2dt*ep;
+    this->dz_dt += this->dz2_2dt*ep;
+    
+    // step positions
+    this->x += this->dx_dt*ep;
+    this->y += this->dy_dt*ep;
+    this->z += this->dz_dt*ep;
+
+    if (this->record && (this->inc++ % rec_decmi == 0)) {
+        this->history_x_coords.push_back(this->x - *origin_x);
+        this->history_y_coords.push_back(this->y - *origin_y);
+        this->history_z_coords.push_back(this->z - *origin_z);
+    }
 }
 
 void celestial::step_position(std::vector<celestial *> c_) {
@@ -136,7 +190,7 @@ void celestial::step_position(std::vector<celestial *> c_) {
     this->y += this->dy_dt*ep;
     this->z += this->dz_dt*ep;
 
-    if (this->inc++ % rec_decmi == 0) {
+    if (this->record && (this->inc++ % rec_decmi == 0)) {
         this->history_x_coords.push_back(this->x);
         this->history_y_coords.push_back(this->y);
         this->history_z_coords.push_back(this->z);
--- main.cpp
+++ main.cpp
@@ -5,30 +5,42 @@
 
 
 int main() {
+    double *origin_x;
+    double *origin_y;
+    double *origin_z;
+
     celestial sun("sun", 1.9885e30, 0, 0, 0, 0, 0, 0); 
 
-    celestial earth("earth", 5.972e24, 150e9, 0, 0, 0, 29.8e3, 0); 
-    celestial moon("moon", 7.342e22, 150e9 + 380e6, 0, 0, 0, 29.8e3 + 1.022e3, 0); 
+    celestial mercury("mercury", sun.m, 3.301e23, 57.91e9); 
+    celestial venus("venus", sun.m, 4.867e24, 108.21e9); 
+    celestial earth("earth", sun.m, 5.972e24, 150e9); 
+    celestial moon("moon", 7.342e22, 150e9 + 380e6, 0, 0, 0, 29.8e3 + 1.02e3, 0); 
+    celestial mars("mars", sun.m, 6.417e23, 227.94e9); 
+    celestial jupiter("jupiter", sun.m, 1.898e27, 778e9); 
+    celestial saturn("saturn", sun.m, 5.683e26, 1433.53e9); 
+    celestial uranus("uranus", sun.m, 8.681e25, 2870.97e9); 
+    celestial neptune("neptune", sun.m, 1.024e26, 4500e9); 
 
-    celestial jupiter("jupiter", 1.898e27, 778e9, 0, 0, 0, 13.07e3, 0); 
+    origin_x = &earth.x;
+    origin_y = &earth.y;
+    origin_z = &earth.z;
 
-    std::vector<celestial *> objs = {&earth, &moon, &sun, &jupiter}; 
-
-    // celestial earth(5.972e24, 0, 0, 0, 0, 0, 0); 
-    // celestial moon(7.342e22, 380e6, 0, 0, 0, 1.022e3, 0); 
-
+    std::vector<celestial *> objs = {&sun, &mercury, &venus, &earth, &moon, &mars, &jupiter, &saturn, &uranus, &neptune}; 
     // std::vector<celestial *> objs = {&earth, &moon}; 
     
     for (int i = 0; i < 100000; i++) {
         for (celestial *obj : objs) {
             obj->step_position(objs);
+            // obj->step_position(objs, origin_x, origin_y, origin_z);
         }
     }
 
     // Create a plot
     plt::figure();
     for (celestial *obj : objs) {
-        plt::plot(obj->history_x_coords, obj->history_y_coords, {{"label", obj->name}});
+        if (obj->record) {
+            plt::plot(obj->history_x_coords, obj->history_y_coords, {{"label", obj->name}});
+        }
     }
     plt::title("Sample Plot");
     plt::xlabel("X-axis");
